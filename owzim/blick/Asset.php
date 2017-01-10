ab61f717cf816fae57130356b7dbc5e25227c443
Christian Raunitschka <git@raunitschka.de>
2017-01-10 20:48:39 +0100

    Convert relative urls

--- owzim/blick/Asset.php
+++ owzim/blick/Asset.php
@@ -6,7 +6,7 @@
  * @author Christian (owzim) Raunitschka <git@raunitschka.de>
  * @copyright Copyright (c) 2017, Christian Raunitschka
  *
- * @version 0.5.1
+ * @version 0.5.2
  *
  * @filesource
  *
@@ -304,11 +304,11 @@ class Asset extends BlickWireData
         if (!$this->isRemote) {
             $filePrefix = $this->filePrefix ? "{$this->filePrefix}/" : '';
             $variationSubDir = $this->variationSubDir ? "{$this->variationSubDir}/" : '';
-            return "{$this->urlPrefix}/{$filePrefix}{$variationSubDir}{$this->filename}{$this->param}";
+            $rtn = "{$this->urlPrefix}/{$filePrefix}{$variationSubDir}{$this->filename}{$this->param}";
         } else {
-            return $this->_fullName;
+            $rtn = $this->_fullName;
         }
-
+        return $this->removeDotPathSegments($rtn);
     }
 
     /**
@@ -450,5 +450,83 @@ class Asset extends BlickWireData
     protected function getVersion()
     {
         return file_exists($this->path) ? filemtime($this->path) : '';
+    }
+
+    // https://gist.github.com/rdlowrey/5f56cc540099de9d5006
+    public function removeDotPathSegments($path)
+    {
+        if (strpos($path, '.') === false) {
+            return $path;
+        }
+        $inputBuffer = $path;
+        $outputStack = [];
+        /**
+         * 2.  While the input buffer is not empty, loop as follows:
+         */
+        while ($inputBuffer != '') {
+            /**
+             * A.  If the input buffer begins with a prefix of "../" or "./",
+             *     then remove that prefix from the input buffer; otherwise,
+             */
+            if (strpos($inputBuffer, "./") === 0) {
+                $inputBuffer = substr($inputBuffer, 2);
+                continue;
+            }
+            if (strpos($inputBuffer, "../") === 0) {
+                $inputBuffer = substr($inputBuffer, 3);
+                continue;
+            }
+            /**
+             * B.  if the input buffer begins with a prefix of "/./" or "/.",
+             *     where "." is a complete path segment, then replace that
+             *     prefix with "/" in the input buffer; otherwise,
+             */
+            if ($inputBuffer === "/.") {
+                $outputStack[] = '/';
+                break;
+            }
+            if (substr($inputBuffer, 0, 3) === "/./") {
+                $inputBuffer = substr($inputBuffer, 2);
+                continue;
+            }
+            /**
+             * C.  if the input buffer begins with a prefix of "/../" or "/..",
+             *     where ".." is a complete path segment, then replace that
+             *     prefix with "/" in the input buffer and remove the last
+             *     segment and its preceding "/" (if any) from the output
+             *     buffer; otherwise,
+             */
+            if ($inputBuffer === "/..") {
+                array_pop($outputStack);
+                $outputStack[] = '/';
+                break;
+            }
+            if (substr($inputBuffer, 0, 4) === "/../") {
+                array_pop($outputStack);
+                $inputBuffer = substr($inputBuffer, 3);
+                continue;
+            }
+            /**
+             * D.  if the input buffer consists only of "." or "..", then remove
+             *     that from the input buffer; otherwise,
+             */
+            if ($inputBuffer === '.' || $inputBuffer === '..') {
+                break;
+            }
+            /**
+             * E.  move the first path segment in the input buffer to the end of
+             *     the output buffer, including the initial "/" character (if
+             *     any) and any subsequent characters up to, but not including,
+             *     the next "/" character or the end of the input buffer.
+             */
+            if (($slashPos = stripos($inputBuffer, '/', 1)) === false) {
+                $outputStack[] = $inputBuffer;
+                break;
+            } else {
+                $outputStack[] = substr($inputBuffer, 0, $slashPos);
+                $inputBuffer = substr($inputBuffer, $slashPos);
+            }
+        }
+        return implode($outputStack);
     }
 }
